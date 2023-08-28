fb98dcba3760e1167d6825dc7d6ea53780f0d3b3
JFMajer <jakubmajer@live.com>
2023-08-28 20:04:18 +0200

    first commit

--- lib/s3-bucket-stack.ts
+++ lib/s3-bucket-stack.ts
@@ -1,16 +1,56 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Bucket, CfnBucket } from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+class L3Bucket extends Construct {
+  constructor(scope: Construct, id: string, expirationInDays?: number) {
+    super(scope, id);
+
+    new Bucket(this, "MyL3Bucket", {
+      lifecycleRules: [
+        {
+          expiration: cdk.Duration.days(expirationInDays || 2),
+        },
+      ],
+    });
+  }
+}
 export class S3BucketStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const randomNum = generateRandomNumber(10000000, 99999999);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'S3BucketQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // creates s3 bucket using l2 construct
+    new Bucket(this, "MyL2Bucket", {
+      versioned: true,
+      bucketName: `my-l2-bucket-${randomNum}`,
+      lifecycleRules: [
+        {
+          expiration: cdk.Duration.days(2),
+        },
+      ],
+    });
+
+    // creates s3 bucket using l1 construct
+    new CfnBucket(this, "MyL1Bucket", {
+      lifecycleConfiguration: {
+        rules: [
+          {
+            expirationInDays: 2,
+            status: "Enabled",
+          },
+        ],
+      },
+    });
+
+    // creates s3 bucket using l3 construct
+    new L3Bucket(this, "MyL3Bucket", 3);
+
   }
+}
+
+function generateRandomNumber(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
