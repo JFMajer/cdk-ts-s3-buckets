2986e42f5c8348547685ebee7a5750dc3540c6fd
JFMajer <jakubmajer@live.com>
2023-09-08 20:00:28 +0200

    typo

---
+++ lib/PhotosHandlerStack.ts
@@ -0,0 +1,40 @@
+import * as cdk from "aws-cdk-lib";
+import { Function as LambdaFunction } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import { Fn } from "aws-cdk-lib";
+import { Runtime, Code } from "aws-cdk-lib/aws-lambda";
+
+
+const appName: string = "photos";
+export class PhotosHandlerStack extends cdk.Stack {
+  private stackSuffix: string;
+
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    this.generateStackSuffix();
+
+    const targetBucket = Fn.importValue('photos-bucket');
+    
+    const PhotosHandlerFunction = new LambdaFunction(this, 'PhotosHandler', {
+      runtime: Runtime.NODEJS_18_X,
+      handler: "index.handler",
+      functionName: `photos-handler-${appName}-${this.stackSuffix}`,
+      code: Code.fromInline(`
+        exports.handler = async (event) => {
+          console.log("hello from lambda! linked bucket arn is: " + process.env.TARGET_BUCKET);
+        }
+      `),
+      environment: {
+        TARGET_BUCKET: targetBucket,
+      }
+    });
+    
+    
+  }
+
+  private generateStackSuffix() {
+    const shortStackId = Fn.select(2, Fn.split("/", this.stackId));
+    this.stackSuffix = Fn.select(4, Fn.split("-", shortStackId));
+  }
+}
